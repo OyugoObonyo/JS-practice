a65f807822a3ed8058172ecf8357caa29d5b47e5
OyugoObonyo <nyikal1999@gmail.com>
2022-02-22 12:26:08 +0300

    complete object literal exercise

--- INTRODUCTION/app.js
+++ INTRODUCTION/app.js
@@ -1,15 +1,12 @@
-console.log("Connecting Script with HTML document");
-const age = prompt("What's your age ")
-switch (age) {
-    case '18':
-        alert("Legal age");
-        break;
-    case '17':
-        alert("close enough");
-        break;
-    case '19':
-        alert("1 step ahead");
-        break;
-    default:
-        alert("Age not recognized")
+let output = []
+let num = 0
+// creating object literals
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
 }
+let fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state}, ${restaurant.zipcode}`
+alert(fullAddress)
