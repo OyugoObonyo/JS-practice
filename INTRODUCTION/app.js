1240cf27da09eed8b47d5d590e16c95e4d4aef67
OyugoObonyo <nyikal1999@gmail.com>
2022-02-23 16:40:32 +0300

    Explore map and arrow functions

--- INTRODUCTION/app.js
+++ INTRODUCTION/app.js
@@ -1,8 +1,4 @@
-// exploring the this keyword
-const hen = {
-    name : "Helen",
-    eggCount : 0,
-    layAnEgg() {
-        this.eggCount += 1
-    }
-}
+// exploring the map function
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
+const firstNames = fullNames.map( name => name.first )
+console.log(`Using the map function: ${firstNames}`)
