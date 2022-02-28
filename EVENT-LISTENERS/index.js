23ee20ba501c48f0b55938ac1d7502301360bdbf
OyugoObonyo <nyikal1999@gmail.com>
2022-02-28 11:53:01 +0300

    Implement listeners with switchcase

--- EVENT-LISTENERS/index.js
+++ EVENT-LISTENERS/index.js
@@ -5,42 +5,38 @@ for(button of buttons ) {
     button.addEventListener('click', function() {
         this.style.color = "white"
         let action = this.innerHTML;
-        console.log(action)
-        if(action === 'w') {
-            let audio = new Audio('sounds/tom-1.mp3')
-            audio.play()
+        switch(action){
+            case 'w':
+                let tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+            case 'a':
+                let tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+            case 's':
+                let tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+            case 'd':
+                let tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+            case 'j':
+                let snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+            case 'k':
+                let crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+            case 'l':
+                let kickBass = new Audio('sounds/kick-bass.mp3');
+                kickBass.play();
+                break;      
+            default:
+                alert("Error")
+            }
         }
-        else if(action === 'a') {
-            let audio = new Audio('sounds/tom-2.mp3')
-            audio.play()
-        }
-        else if(action === 's') {
-            let audio = new Audio('sounds/tom-3.mp3')
-            audio.play()
-        }
-        else if(action === 'd') {
-            let audio = new Audio('sounds/tom-4.mp3')
-            audio.play()
-        }
-        else if(action === 'j') {
-            let audio = new Audio('sounds/snare.mp3')
-            audio.play()
-        }
-        else if(action === 'k') {
-            let audio = new Audio('sounds/crash.mp3')
-            audio.play()
-        }
-        else if(action === 'l') {
-            let audio = new Audio('sounds/kick-bass.mp3')
-            audio.play()
-        }
-    })
+    )
 }
-    
-
-
-
-//    console.log(button)
-//    button.addEventListener("click", function(){
-//        console.log(button);
-//    })}
