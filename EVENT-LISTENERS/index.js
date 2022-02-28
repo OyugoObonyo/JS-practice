b9ff4819876d1ffaadfa8bf29966d1329bdd4902
OyugoObonyo <nyikal1999@gmail.com>
2022-02-28 12:46:27 +0300

    Understanding keyboard event listeners

--- EVENT-LISTENERS/index.js
+++ EVENT-LISTENERS/index.js
@@ -1,42 +1,43 @@
-// Playing sound in JS
-// using if else statements to acccess each button
-buttons = document.querySelectorAll('.drum');
-for(button of buttons ) {
-    button.addEventListener('click', function() {
-        this.style.color = "white"
-        let action = this.innerHTML;
-        switch(action){
-            case 'w':
-                let tom1 = new Audio('sounds/tom-1.mp3');
-                tom1.play();
-                break;
-            case 'a':
-                let tom2 = new Audio('sounds/tom-2.mp3');
-                tom2.play();
-                break;
-            case 's':
-                let tom3 = new Audio('sounds/tom-3.mp3');
-                tom3.play();
-                break;
-            case 'd':
-                let tom4 = new Audio('sounds/tom-4.mp3');
-                tom4.play();
-                break;
-            case 'j':
-                let snare = new Audio('sounds/snare.mp3');
-                snare.play();
-                break;
-            case 'k':
-                let crash = new Audio('sounds/crash.mp3');
-                crash.play();
-                break;
-            case 'l':
-                let kickBass = new Audio('sounds/kick-bass.mp3');
-                kickBass.play();
-                break;      
-            default:
-                alert("Error")
-            }
-        }
-    )
-}
+// Exploring Keypress event listeners in JS 
+document.addEventListener("keypress", function(krr) {
+    alert(`${krr.key} was pressed`)
+})
+// for(button of buttons ) {
+//     button.addEventListener('click', function() {
+//         this.style.color = "white"
+//         let action = this.innerHTML;
+//         switch(action){
+//             case 'w':
+//                 let tom1 = new Audio('sounds/tom-1.mp3');
+//                 tom1.play();
+//                 break;
+//             case 'a':
+//                 let tom2 = new Audio('sounds/tom-2.mp3');
+//                 tom2.play();
+//                 break;
+//             case 's':
+//                 let tom3 = new Audio('sounds/tom-3.mp3');
+//                 tom3.play();
+//                 break;
+//             case 'd':
+//                 let tom4 = new Audio('sounds/tom-4.mp3');
+//                 tom4.play();
+//                 break;
+//             case 'j':
+//                 let snare = new Audio('sounds/snare.mp3');
+//                 snare.play();
+//                 break;
+//             case 'k':
+//                 let crash = new Audio('sounds/crash.mp3');
+//                 crash.play();
+//                 break;
+//             case 'l':
+//                 let kickBass = new Audio('sounds/kick-bass.mp3');
+//                 kickBass.play();
+//                 break;      
+//             default:
+//                 alert("Error")
+//             }
+//         }
+//     )
+// }
