f730838de72894c7ba9e8c67289199ca1b1f4173
OyugoObonyo <nyikal1999@gmail.com>
2022-02-28 11:33:27 +0300

    using if else statement to apply event listener

--- EVENT-LISTENERS/index.js
+++ EVENT-LISTENERS/index.js
@@ -1,6 +1,46 @@
-buttons = document.querySelectorAll('button')
-for (button of buttons ) {
-    // Use anonymous functions
-    button.addEventListener("click", function(){
-        alert("Button was clicked!")
-    })}
+// Playing sound in JS
+// using if else statements to acccess each button
+buttons = document.querySelectorAll('.drum');
+for(button of buttons ) {
+    button.addEventListener('click', function() {
+        this.style.color = "white"
+        let action = this.innerHTML;
+        console.log(action)
+        if(action === 'w') {
+            let audio = new Audio('sounds/tom-1.mp3')
+            audio.play()
+        }
+        else if(action === 'a') {
+            let audio = new Audio('sounds/tom-2.mp3')
+            audio.play()
+        }
+        else if(action === 's') {
+            let audio = new Audio('sounds/tom-3.mp3')
+            audio.play()
+        }
+        else if(action === 'd') {
+            let audio = new Audio('sounds/tom-4.mp3')
+            audio.play()
+        }
+        else if(action === 'j') {
+            let audio = new Audio('sounds/snare.mp3')
+            audio.play()
+        }
+        else if(action === 'k') {
+            let audio = new Audio('sounds/crash.mp3')
+            audio.play()
+        }
+        else if(action === 'l') {
+            let audio = new Audio('sounds/kick-bass.mp3')
+            audio.play()
+        }
+    })
+}
+    
+
+
+
+//    console.log(button)
+//    button.addEventListener("click", function(){
+//        console.log(button);
+//    })}
